051d0427e5b5136f397b1fa40e9ed475bfe7540e
M4RI0BR0S <mariuszkrych02@gmail.com>
2023-04-07 19:55:25 +0200

    Create Animbg class and use it in project

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const elements = document.querySelectorAll('.anim-bg')
-  console.log(elements);
-  
-
-  document.addEventListener('mousemove', function (e) { 
-    console.log(e);
-    const { clientX, clientY } = e;
-
-    const centerX = window.innerWidth / 2;
-    const centerY = window.innerHeight / 2;
-    
-    elements.forEach(element => {
-      const ratioX = -element.getAttribute('ratioX');
-      const ratioY = -element.getAttribute('ratioY');
-      const moveX = clientX - centerX; 
-      const moveY = clientY - centerY;
-      element.style.transform = `translate(${moveX * ratioX}px,${moveY * ratioY}px)`
-    })
-   })
-
+  const animgBg = new AnimBg('.anim-bg')
+  document.addEventListener('mousemove',(e)=>animgBg.listenCursorMove(e))
 })
